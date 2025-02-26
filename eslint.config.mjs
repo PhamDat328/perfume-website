a5ec681e146392c249fd43541a5256bf890b6a91
phamdat328 <dpham090909@gmail.com>
2025-02-26 22:22:42 +0700

    config src

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'tailwindcss'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'tailwindcss/classnames-order': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+      extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:tailwindcss/recommended',
+        'prettier',
+        //'plugin:@typescript-eslint/recommended-type-checked',
+        // 'plugin:@typescript-eslint/strict-type-checked',
+        // 'plugin:@typescript-eslint/stylistic-type-checked',
+      ],
+      rules: {
+        'tailwindcss/classnames-order': 'off',
+      },
+    },
+  ],
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
